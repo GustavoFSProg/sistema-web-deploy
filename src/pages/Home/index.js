cc9749905db6a65a55b9e45093b0e35a484cde11
Gustavo <gustavosohne38@gmail.com>
2020-12-12 16:27:54 -0300

    indo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import api from '../../service/api'
 import './style.css'
 import Header from '../Header'
